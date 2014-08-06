e2ed4310d03e44612289fcfa45847295752c31f5
Nickkos <Nickkoskowski@gmail.com>
2014-08-05 21:25:38 -0700

    Added initial HTML files.

---
+++ index.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+	<title>Patrick Herman - Personal Injury Attorney</title>
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="responsive.css">
+	<link href="http://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="reset">
+		<header>
+			<div class="top-bar">
+				<div class="container">
+					<p>Call Now: <span>619-795-5555</span></p>
+				</div>
+			</div>
+			<div class="container">
+				<div class="logo">
+					<h1>Patrick E. Herman</h1>
+					<h2>Attorney at Law</h2>
+				</div>
+				<div class="navigation">
+					<nav>
+						<ul>
+							<li>Home</li>
+							<li>About Me</li>
+							<li>Cases</li>
+							<li>Press</li>
+							<li>Affiliates</li>
+							<li>Contact</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<section>
+			<div class="container">
+				<div class="slider">
+					<img src="images/law-book.png">
+					<div class="overlay">
+						<h2>About Me</h2>
+						<img src="images/logo.jpg">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</p>
+							<button>Contact Me</button>
+						</div>
+					</div>
+					<div class="white-wrap">
+						<div class="col-fw">
+							<h3>Headline Goes Here</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+						<div class="col-hw">
+							<h3>Headline Goes Here</h3>
+							<img src="http://placehold.it/450x150" class="ft-img">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+						<div class="col-hw">
+							<h3>Headline Goes Here</h3>
+							<img src="http://placehold.it/450x150" class="ft-img">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+						<div class="col-third">
+							<h3>Headline Goes Here</h3>
+							<img src="http://placehold.it/250x150" class="ft-img">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+						<div class="col-third">
+							<h3>Headline Goes Here</h3>
+							<img src="http://placehold.it/250x150" class="ft-img">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+						<div class="col-third">
+							<h3>Headline Goes Here</h3>
+							<img src="http://placehold.it/250x150" class="ft-img">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<footer>
+				<div class="container">
+					<p class="textLeft">Copyright 2014, All Rights Reserved <br/> <i>Designed by Sara Herman & Nicholas Koskowski</i></p>
+					<p class="textRight alignRight">619-729-8424<br/>contact@patrickhermanlaw.com<br/>12970 Hwy 8 Bus #126 El Cajon, CA</p>
+				</div>
+			</footer>
+		</div>
+	</body>
+	</html>
